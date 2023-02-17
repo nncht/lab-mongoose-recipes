05c020b9735e969039807edbd3923a1d2937f05b
Nana Chuto <mail@nanachuto.com>
2023-02-17 13:45:45 +0100

    Lab completed.

--- index.js
+++ index.js
@@ -18,51 +18,55 @@ mongoose
   })
   .catch((error) => {
     console.error("Error connecting to the database", error);
-  })
-  .then(() => {
-    const recipe = {
-      title: "Ochazuke",
-      level: "Easy Peasy",
-      ingredients: [
-        "Short-grain rice",
-        "Green tea",
-        "Salmon",
-        "Ochazuke seasoning",
-      ],
-      cuisine: "Japanese",
-      image:
-        "https://www.justonecookbook.com/wp-content/uploads/2020/01/Ochazuke-8958-2-II.jpg",
-      duration: 10,
-      creator: "Nana",
-    };
+  });
 
-    Recipe.create(recipe).then(console.log(recipe.title));
+const recipe = {
+  title: "Ochazuke",
+  level: "Easy Peasy",
+  ingredients: [
+    "Short-grain rice",
+    "Green tea",
+    "Salmon",
+    "Ochazuke seasoning",
+  ],
+  cuisine: "Japanese",
+  image:
+    "https://www.justonecookbook.com/wp-content/uploads/2020/01/Ochazuke-8958-2-II.jpg",
+  duration: 10,
+  creator: "Nana",
+};
+
+const createRecipe = Recipe.create(recipe).then(console.log(recipe.title));
+
+const recipes = data;
+const insertRecipes = Recipe.insertMany(recipes).then(() =>
+  recipes.forEach((recipe) => {
+    console.log(recipe.title);
   })
-  .then(() => {
-    const recipes = data;
-    Recipe.insertMany(recipes);
-    recipes.forEach((recipe) => {
-      console.log(recipe.title);
-    });
-  })
-  .then(() => {
-    Recipe.findOneAndUpdate(
-      { title: "Rigatoni alla Genovese" },
-      { duration: 100 }
-    );
-  })
+);
+
+const updateRigatoni = Recipe.findOneAndUpdate(
+  { title: "Rigatoni alla Genovese" },
+  { duration: 100 }
+)
   .then((x) => {
     console.log("Rigatoni alla Genovese was updated!");
   })
   .catch((error) => {
     console.error("Something went wrong.");
-  })
-  .then(() => {
-    Recipe.deleteOne({ title: "Carrot Cake" });
-  })
+  });
+
+const deleteCarrotCake = Recipe.deleteOne({ title: "Carrot Cake" })
   .then((x) => {
     console.log("Carrot Cake was deleted!");
   })
   .catch((error) => {
     console.error("Something went wrong.");
   });
+
+Promise.all([createRecipe, insertRecipes, updateRigatoni, deleteCarrotCake])
+  .then((values) => {
+    console.log("Completed all requests.");
+    mongoose.connection.close();
+  })
+  .catch((err) => console.error(err));
